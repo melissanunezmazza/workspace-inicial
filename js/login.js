93977bf4b02333fb5db7044f12f0e21af9cb05a4
melissanunezmazza <110545198+melissanunezmazza@users.noreply.github.com>
2022-08-22 09:37:18 -0300

    new

--- js/login.js
+++ js/login.js
@@ -9,7 +9,7 @@ formLog.addEventListener('submit', function(e){
 
     if (user.length && password.length >= 8) {
         e.preventDefault();
-        window.location.href = 'my-profile.html';
+        window.location.href = 'index.html';
     } else if (!(user.length>=8)) {
         e.preventDefault();
         alert('Debe ingresar al menos 8 caracteres');
